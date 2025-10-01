d2a81921eeecedd02424d737dfce1d191770dc66
Musdate <bastiantroncoso97@gmail.com>
2025-10-01 10:10:04 -0500

    [ADD] agGrid Modules

--- src/app/modules/landing/landing.ts
+++ src/app/modules/landing/landing.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "@shared/components/navbar/navbar";
 
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
+ModuleRegistry.registerModules([ AllCommunityModule ]);
+
 @Component({
   selector: 'app-landing',
   imports: [ Navbar, RouterOutlet ],
